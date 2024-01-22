ab23860c426a0f6ad5ce1fec36461f5811f653a2
Siheon Kim <siheon0411@gmail.com>
2024-01-22 20:19:11 +0900

    data fetching, async await

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,16 @@
+import 'package:http/http.dart' as http;
+
 class ApiService {
-  final String baseURL = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
+
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final response =
+        await http.get(url); // 비동기식(async) 프로그래밍: 서버가 응답할때까지 프로그램을 기다리게함
+    if (response.statusCode == 200) {
+      print(response.body);
+    }
+    throw Error();
+  }
 }
