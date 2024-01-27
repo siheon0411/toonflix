821d3bb7f444abaad7aef4894859a0bd9d9b96ec
Siheon Kim <siheon0411@gmail.com>
2024-01-27 15:09:23 +0900

    fromJson, named constructor modeling

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response =
         await http.get(url); // 비동기식(async) 프로그래밍: 서버가 응답할때까지 프로그램을 기다리게함
     if (response.statusCode == 200) {
-      print(response.body);
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(toon);
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
